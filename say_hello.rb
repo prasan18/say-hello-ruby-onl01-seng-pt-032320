4eb19fc50d9ff77894a55c69334cacbd71638858
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-01 03:23:35 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
-def greeting
-puts "Hello Gabriela"  
+def greeting(name="Gabriela!",language="ruby")
+  puts "Hello #{name}.we heard you are a great #{language} programmer."
 end
 greeting
-learn submit_tag
+learn submit
